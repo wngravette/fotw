d30e48d0c0166f3a85774050f7022b317f380300
William Naughton-Gravette <william@n-r.co>
2015-09-11 00:22:27 +1000

    UI minors

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <h1>Get a load of this flog:</h1>
             <h1 class="flog_name">Tom D'ath</h1>
             <p class="flog_stats">Flog #001</p>
-            <p>This flog built this website, and last week he let it go offline for <b>two, whole, fucking days</b>, Absolute flog.</p>
+            <p>This flog is a real piece of work. Just look at his floggish face. The worst part? He knows he's a flog, and he embraces the flog lifestyle.</p>
         </div>
     </div>
     <div class="pure-u-1 pure-u-md-16-24 flog_hero">
