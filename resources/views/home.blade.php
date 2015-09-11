f42698cd5b91576ca517c7736988d26a08e49ebc
William Naughton-Gravette <william@n-r.co>
2015-09-12 00:57:03 +1000

    UI + S3 integration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,20 @@
 <div class="pure-g">
     <div class="pure-u-1 pure-u-md-8-24 flog_hero_text">
         <div class="l-box">
-            <h1>Get a load of this flog:</h1>
-            <h1 class="flog_name">Tom D'ath</h1>
-            <p class="flog_stats">Flog #001</p>
-            <p>This flog is a real piece of work. Just look at his floggish face. The worst part? He knows he's a flog, and he embraces the flog lifestyle.</p>
+            <h1 class="subheading">Get a load of this flog:</h1>
+            <h1 class="big">{!! $current_flog->name !!}</h1>
+            <p class="flog_stats">Flog #{{$current_flog->id}}</p>
+            <p>{{$current_flog->reason}}</p>
         </div>
     </div>
     <div class="pure-u-1 pure-u-md-16-24 flog_hero">
-        <img class="pure-img" src="{{asset('img/flog2.jpg')}}"/>
+        <img class="pure-img" src="https://s3-ap-southeast-2.amazonaws.com/fotw/{{$current_flog->flog_hash}}.jpg"/>
     </div>
 </div>
+<div class="pure-g">
+    <div class="pure-u-10-24 prev_flogs">
+        <div class="l-box">
+            <h1 class="subheading">Previous F'sOTW</h1>
+        </div>
+    </div>
 @endsection
