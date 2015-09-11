b1189b87eb7a993abf2fc33fae1a4fb0d6382a92
William Naughton-Gravette <william@n-r.co>
2015-09-12 00:57:19 +1000

    fetch current flog and style ID number

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Flog;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -11,6 +13,8 @@ class HomeController extends Controller
 {
     public function render()
     {
-        return view('home');
+        $currentFlog = Flog::orderBy('id', 'desc')->first();
+        $currentFlog->id = str_pad($currentFlog->id, 3, '0', STR_PAD_LEFT);
+        return view('home', ['current_flog' => $currentFlog]);
     }
 }
