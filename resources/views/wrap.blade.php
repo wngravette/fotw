ad95907171d52eacf97474529b8d124b9caa0997
William Naughton-Gravette <william@n-r.co>
2015-09-13 01:57:00 +1000

    UI minors

--- resources/views/wrap.blade.php
+++ resources/views/wrap.blade.php
@@ -7,9 +7,10 @@
           <link rel="stylesheet" href="http://yui.yahooapis.com/combo?pure/0.6.0/base-min.css&pure/0.6.0/grids-min.css&pure/0.6.0/grids-responsive-old-ie-min.css">
         <![endif]-->
         <!--[if gt IE 8]><!-->
-          <link rel="stylesheet" href="http://yui.yahooapis.com/combo?pure/0.6.0/base-min.css&pure/0.6.0/grids-min.css&pure/0.6.0/grids-responsive-min.css">
+          <link rel="stylesheet" href="http://yui.yahooapis.com/combo?pure/0.6.0/base-min.css&pure/0.6.0/grids-min.css&pure/0.6.0/grids-responsive-min.css&pure/0.6.0/buttons-min.css">
         <!--<![endif]-->
         <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
+        <link rel="stylesheet" href="{{asset('css/buttons.css')}}"/>
         <script src="https://use.typekit.net/hci6zrn.js"></script>
         <script>try{Typekit.load({ async: true });}catch(e){}</script>
         @yield('additional_head')
@@ -18,7 +19,7 @@
         <div class="container">
             <div class="pure-u-1">
                 <div class="l-box">
-                    <p><span class="logo">FLOG OF THE WEEK</span> <span class="current_flog">Week of: <span>7th of September</span></span></p>
+                    <p><span class="logo">FLOG OF THE WEEK</span> <span class="current_flog">Week of: <span>7th September</span></span></span></p>
                 </div>
             </div>
             @yield('content')
