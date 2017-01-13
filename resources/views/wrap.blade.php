25103ba55eac857b59111ea7e252c50d8f23c9d7
William Naughton-Gravette <william@willng.me>
2017-01-14 00:03:52 +1100

    prevent couple-tap zoom on buttons

--- resources/views/wrap.blade.php
+++ resources/views/wrap.blade.php
@@ -26,6 +26,24 @@
         <script src="https://use.typekit.net/hci6zrn.js"></script>
         <script>try{Typekit.load({ async: true });}catch(e){}</script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.0/Chart.min.js"></script>
+        <script>
+        (function($) {
+          $.fn.nodoubletapzoom = function() {
+              $(this).bind('touchstart', function preventZoom(e) {
+                var t2 = e.timeStamp
+                  , t1 = $(this).data('lastTouch') || t2
+                  , dt = t2 - t1
+                  , fingers = e.originalEvent.touches.length;
+                $(this).data('lastTouch', t2);
+                if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+                e.preventDefault(); // double tap - prevent the zoom
+                // also synthesize click events we just swallowed up
+                $(this).trigger('click').trigger('click');
+              });
+          };
+        })(jQuery);
+        </script>
         @yield('additional_head')
     </head>
     <body>
