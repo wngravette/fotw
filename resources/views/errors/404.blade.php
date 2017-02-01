21ea2979427cd7b0db1ddd1925708ca8c6689ff1
William‌‌ Naughton-Gravette <william@willng.me>
2017-02-01 18:14:15 +1100

    Add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Be right back.</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://use.typekit.net/hci6zrn.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+        <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: black;
+                display: table;
+                font-family: brandon-grotesque, sans-serif;
+                font-weight: 800;
+                font-style: italic;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 25px;
+                margin-bottom: 40px;
+            }
+
+            .sub {
+                font-family: proxima-nova, sans-serif;
+                font-size: 12px;
+                margin-top: 20px;
+                font-style: normal;
+                font-weight: 300;
+            }
+
+            i.em {
+                vertical-align: -8px;
+                margin-right: 4px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">FOTW <i class="em em-astonished"></i><i class="em em-ambulance"></i><i class="em em-pray"></i></div>
+                <div class="title">we're down but we're working on it*</div>
+                <div class="sub">* we're probably not working on it but tbh who cares</div>
+            </div>
+        </div>
+    </body>
+</html>
